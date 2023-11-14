cbf5b3d8b007e94656c4d346de8e2d55788c7b22
suhailkhazirbhat <suhailbhatb7@gmail.com>
2023-11-14 23:09:27 +0530

    nav

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+// import React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import HeaderContainer from './containers/HeaderContainer';
+import HomeContainer from './containers/HomeContainer'
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function App() {
+  return ( <Grid item xs={12}>
+    <HeaderContainer />
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        
+        <Grid item xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>  <HomeContainer /></Item>
+        </Grid>
+      </Grid>
+    </Box>
+    </Grid>);
 }
-
-export default App;
